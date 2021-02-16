e2469d49b4af764cf6c3ea737f0b4bed3cde6bff
joshua mshana <mama27j@gmail.com>
2021-02-16 23:02:12 +0300

    add users api

---
+++ functions/apis/users.mjs
@@ -0,0 +1,55 @@
+import bfastnode from "bfastnode";
+
+const { bfast } = bfastnode;
+
+export const getUser = bfast.functions().onGetHttpRequest(
+    '/users/:mobile',
+    (request, response) => {
+        const mobile = request.params.mobile;
+        bfast.database().table('users').get(mobile).then(value => {
+            if (value && !Array.isArray(value)) {
+                response.status(200).json(value);
+            } else {
+                response.status(404).json({ message: 'no such user' });
+            }
+        }).catch(reason => {
+            // if(reason && reason.message && reason.message.toString().trim() === 'Query not succeed'){
+            //     return bfast.database().table('users').save(body).then(value=>{
+            //         response.status(200).json(value);
+            //     }).catch(reason=>{
+            //         response.status(400).json(reason);
+            //     });
+            // }else{
+            response.status(400).json(reason);
+            // }
+        });
+    }
+);
+
+export const registerUser = bfast.functions().onPostHttpRequest(
+    '/users/:mobile',
+    (request, response) => {
+        const mobile = request.params.mobile;
+        const body = JSON.parse(JSON.stringify(request.body));
+        bfast.database().table('users').get(mobile).then(user => {
+            return bfast.database().table('users').query().byId(user.id).updateBuilder()
+                .raw({
+                    "$set": body
+                })
+                .update();
+        }).then(value => {
+            response.status(200).json(value);
+        }).catch(reason => {
+            if(reason && reason.message && reason.message.toString().trim() === 'Query not succeed'){
+                const _body = Object.assign(body, {id: mobile});
+                return bfast.database().table('users').save(body).then(value=>{
+                    response.status(200).json(value);
+                }).catch(reason=>{
+                    response.status(400).json(reason);
+                });
+            }else{
+                response.status(400).json(reason);
+            }
+        });
+    }
+)
--- functions/index.mjs
+++ functions/index.mjs
@@ -2,6 +2,12 @@ import bfastnode from 'bfastnode';
 
 const {bfast} = bfastnode;
 
+bfast.init({
+    applicationId: 'kazisquare',
+    projectId: 'kazisquare',
+    appPassword: 'kazisquare2021'
+})
+
 export const home = bfast.functions().onHttpRequest('/', (request, response) => {
     response.json({message: 'KaziSquare public API'});
 });
