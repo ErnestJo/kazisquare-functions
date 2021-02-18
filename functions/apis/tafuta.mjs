5ba756e9aa48b2311804bc9e8a9db9a84c0f4ef7
joshua mshana <mama27j@gmail.com>
2021-02-18 14:56:08 +0300

    update learn endpoint

---
+++ functions/apis/tafuta.mjs
@@ -0,0 +1,25 @@
+import bfastnode from "bfastnode";
+const {bfast} = bfastnode;
+
+export const searchWikipedia = bfast.functions().onGetHttpRequest(
+    '/learn',
+    (request, respose)=>{
+        const query = request.query.q;
+        const searchUrl = `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=${encodeURIComponent(query)}`
+        bfast.functions().request(searchUrl).get().then(value=>{
+           if(value && value.query && value.query.pages && typeof value.query.pages === 'object'){
+               const _jibu = Object.keys(value.query.pages).map(x=> value.query.pages[x]? value.query.pages[x].extract: 'Hamna jibu kwa sasa jaribu kitu kingine').join(' ').trim();
+               respose.status(200).json({
+                   jibu: _jibu !== ''?_jibu: 'Hamna jibu kwa sasa jaribu kitu kingine'
+               });
+           }else{
+            respose.status(200).json({
+                jibu: "Hamna jibu kwa sasa jaribu kitu kingine"
+            });
+           }
+        }).catch(reason=>{
+            console.log(reason);
+            respose.status(400).send(value);
+        })
+    }
+);
