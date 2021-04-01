da3e4005fea0362b4f815c3e3398fa8aeeafdf49
joshua mshana <mama27j@gmail.com>
2021-04-01 17:22:07 +0300

    update get works

--- functions/apis/works.mjs
+++ functions/apis/works.mjs
@@ -26,29 +26,11 @@ export const addWork = bfast.functions().onPostHttpRequest(
     }
 );
 
-export const getWorks = bfast.functions().onGetHttpRequest(
-    '/works/:type',
+export const getWorksByCategoryV2 = bfast.functions().onGetHttpRequest(
+    '/works/:category',
     (request, response) => {
-        let workType = request.params.type;
-        let skip = request.query.skip ? request.query.skip : 0;
-        let size = request.query.size ? request.query.size : 8;
-        let page = request.query.page ? request.query.page : 0;
-        // if(Number.isNaN(page)){
-        page = parseInt(page);
-        // }
-        // if(Number.isNaN(skip)){
-        skip = parseInt(skip);
-        // }
-        // if(Number.isNaN(size)){
-        size = parseInt(size);
-        // }
-        if (workType.toString().trim() === '1') {
-            workType = 'online';
-        } else {
-            workType = 'physical';
-        }
         bfast.database().table('works').query()
-            .equalTo("type", workType)
+            .equalTo("category", request.params.category)
             .orderBy("_created_at", -1)
             .size(size)
             .skip(skip)
