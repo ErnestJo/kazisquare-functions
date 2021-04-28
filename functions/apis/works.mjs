5a70ebcc51c24b0bda474f718dac03945e562f87
joshua mshana <mama27j@gmail.com>
2021-04-28 16:27:29 +0300

    add send message to work owner when owner pick a work

--- functions/apis/works.mjs
+++ functions/apis/works.mjs
@@ -119,27 +119,33 @@ export const selectWorkToDo = bfast.functions().onPostHttpRequest(
                 // .get(userWorkChoices[uuid][workId])
                 .then(value => {
                     response.status(200).json(value);
-                    bfast.functions().request('https://rapidpro.ilhasoft.mobi/api/v2/broadcasts.json').post({
-                        "urns": value.owner.urns,
-                        "text": [
-                            `Kazi yako ${value.name} imechaguliwa na ${user.name} mwenye maelezo yafuatayo. `,
-                            Object.keys(user.field).map(key => {
-                                return `${key}: ${user.field[key]}`
-                            }).join(', ')
-                        ].join('')
-                    }, {
-                        headers: {
-                            Authorization: 'Token 991397e43c4fc46db6f902600132103c149cbeeb',
-                            'content-type': 'application/json'
-                        }
-                    }).then(value => {
-                        console.log(value, ': sms imetumwa kwa mwenye kazi.')
-                    }).catch(_234 => {
-                        console.log(_234, ': imeshindwa tuma sms kwa mwenye kazi.');
-                    });
-                }).catch(reason => {
-                response.status(400).send(reason);
-            });
+                    try {
+                        bfast.functions().request('https://rapidpro.ilhasoft.mobi/api/v2/broadcasts.json')
+                            .post({
+                                "urns": value.owner.urns,
+                                "text": [
+                                    `Kazi yako ${value.name} imechaguliwa na ${user.name} mwenye maelezo yafuatayo. `,
+                                    Object.keys(user.field).map(key => {
+                                        return `${key}: ${user.field[key]}`
+                                    }).join(', ')
+                                ].join('')
+                            }, {
+                                headers: {
+                                    Authorization: 'Token 991397e43c4fc46db6f902600132103c149cbeeb',
+                                    'content-type': 'application/json'
+                                }
+                            }).then(value => {
+                            console.log(value, ': sms imetumwa kwa mwenye kazi.')
+                        }).catch(_234 => {
+                            console.log(_234, ': imeshindwa tuma sms kwa mwenye kazi.');
+                        });
+                    } catch (e) {
+                        console.log(e, "***************");
+                    }
+                })
+                .catch(reason => {
+                    response.status(400).send(reason);
+                });
         } else {
             response.status(400).send({message: "no choices for you"});
         }
