7d2f88a3c4c9f33db354c3fc3f815f63a0788692
joshua mshana <mama27j@gmail.com>
2021-05-16 17:47:25 +0300

    update save user and retrieve user details

--- functions/apis/users.mjs
+++ functions/apis/users.mjs
@@ -1,16 +1,20 @@
 import bfastnode from "bfastnode";
 
-const { bfast } = bfastnode;
+const {bfast} = bfastnode;
 
-export const getUser = bfast.functions().onGetHttpRequest(
-    '/users/:mobile',
+export const getUserv2 = bfast.functions().onGetHttpRequest(
+    '/users/:uuid',
     (request, response) => {
-        const mobile = request.params.mobile;
-        bfast.database().table('users').get(mobile).then(value => {
-            if (value && !Array.isArray(value) && value.name && value.age) {
-                response.status(200).json(value);
+        const uuid = request.params.uuid;
+        bfast.database().table('_User').get(uuid, {useMasterKey: true}).then(value => {
+            if (value && !Array.isArray(value) && value.name) {
+                delete value.username;
+                delete value.role;
+                delete value.password;
+                delete value.email;
+                response.status(200).json({user: value});
             } else {
-                response.status(404).json({ message: 'registration not completed' });
+                response.status(200).json({user: 'hamna'});
             }
         }).catch(reason => {
             console.log(reason);
@@ -20,33 +24,31 @@ export const getUser = bfast.functions().onGetHttpRequest(
 );
 
 export const registerUser = bfast.functions().onPostHttpRequest(
-    '/users/:uuid',
+    '/users',
     (request, response) => {
-        const uuid = request.params.uuid;
         const body = request.body;
-        bfast.database().table('users').get(uuid).then(user => {
-            return bfast.database().table('users')
-                .query()
-                .byId(user.id)
-                .updateBuilder()
-                .doc(body)
-                .update();
-        }).then(value => {
-            response.status(200).json(value);
-        }).catch(reason => {
-            console.log(reason);
-            if(reason && reason.message && reason.message.toString().trim() === 'Query not succeed'){
-                body.id = uuid;
-                // console.log()
-                return bfast.database().table('users').save(body).then(value=>{
-                    response.status(200).json(value);
-                }).catch(reason=>{
-                    console.log(reason);
-                    response.status(400).json(reason);
-                });
-            }else{
+        if (body && body.uuid) {
+            body.id = body.uuid;
+            body.email = `${body.id}@kazisquare.co.tz`;
+            bfast.database().table('users').get(body.id).then(user => {
+                if (user) {
+                    return bfast.database().table('_User')
+                        .query()
+                        .byId(user.id)
+                        .updateBuilder()
+                        .doc(body)
+                        .update({useMasterKey: true});
+                } else {
+                    return bfast.auth().signUp(body.id, body.id, body);
+                }
+            }).then(value => {
+                response.status(200).json(value);
+            }).catch(reason => {
+                console.log(reason);
                 response.status(400).json(reason);
-            }
-        });
+            });
+        } else {
+            response.status(400).json({message: 'invalid data'});
+        }
     }
-)
+);
